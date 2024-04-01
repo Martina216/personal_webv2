30298c18a9bbea1b1885f9ca9f056ed63086012a
TINABF\TBF <10230260c@gmail.com>
2024-04-01 22:07:05 +0800

    modify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  script: {
-    "deploy": "gh-pages --dotfiles -d .output/public",
-  },
   app: {
     baseURL: process.env.NODE_ENV === 'production' ? '/personal_webv2/' : '/',
     buildAssetsDir: '/static/'
